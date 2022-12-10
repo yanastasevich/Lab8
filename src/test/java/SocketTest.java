724c0c09cb703db370e230053e556bbdfc86b946
yanastasevich <ioannstasevich1@gmail.com>
2022-12-10 13:24:16 +0100

    add some socket communication

---
+++ src/test/java/SocketTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SocketTest {
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+    }
+}
